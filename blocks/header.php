700c4f8111ddad6f091b7abeeef214832ccec7cc
Oleg Frolov <oleg_frolov@mail.ru>
2021-08-27 22:49:43 +0300

    update

--- blocks/header.php
+++ blocks/header.php
@@ -2,6 +2,23 @@
     <div class="alert alert-info">
         Смотри в /blocks/header.php
     </div>
+    <div>
+        <wb-include wb-src="/modules/yonger/common/blocks/common.inc.php" />
+    </div>
+    <div class="divider-text">Меню сайта</div>
+    <wb-multiinput name="sitemenu">
+        <div class="col">
+            <input class="form-control" type="text" name="label" placeholder="Пункт">
+        </div>
+        <div class="col">
+            <input class="form-control" type="text" name="link" placeholder="Ссылка">
+        </div>
+        <div class="col-auto">
+            <input type="checkbox" class="wd-20 ht-20 m-2" name="active" />
+        </div>
+
+    </wb-multiinput>
+
 </edit>
 <view>
   <loader class="fade out">
@@ -25,25 +42,14 @@
                 <img data-src="/module/myicons/menu-burger.svg?size=30&stroke=333333">
             </button>
             <div class="collapse navbar-collapse order-2" id="navbarNav">
-                <ul class="navbar-nav tx-bold tx-uppercase mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/shop">Ежедневное меню</a>
-                    </li>
-
-                    <li class="nav-item"><a class="nav-link" href="/desserts">
-                            Полезные десерты
-                        </a></li>
-                    <li class="nav-item"><a class="nav-link" href="/catering">
-                            Кейтеринг
-                        </a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">
-                            Доставка
-                        </a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">
-                            Контакты
-                        </a></li>
-                </ul>
                 
+                <ul class="navbar-nav tx-bold tx-uppercase mx-auto">
+                    <wb-foreach wb="from=sitemenu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{link}}">{{label}}</a>
+                    </li>
+                    </wb-foreach>
+                </ul>
                 <div class="d-inline my-2 my-lg-0" id="headsign">
                     <div auto data-ajax="{'url':'/module/yonger/block/headsign','html':'#headsign'}"></div>
                 </div>
---
+++ blocks/map.php
@@ -0,0 +1,61 @@
+<edit header="Яндекс карта">
+    <div class="alert alert-info">
+        Смотри в /blocks/map.php
+    </div>
+    <div>
+        <wb-include wb-src="/modules/yonger/common/blocks/common.inc.php" />
+    </div>
+    <div class="form-group row">
+            <div class="col-12">
+                <label class="form-control-label">Текст</label>
+                <textarea name="text" class="form-control" rows="auto" placeholder="Текст"></textarea>
+            </div>
+        </div>
+
+    <div class="form-group row">
+            <label class="col-3 form-control-label">Zoom</label>
+            <div class="col">
+            <input type="number" name="zoom" min="1" max="18" class="form-control" placeholder="Zoom (1-18)">
+            </div>
+    </div>
+
+    <div class="form-group row">
+            <label class="col-3 form-control-label">Высота</label>
+            <div class="col">
+                <input type="number" name="height" min="100" class="form-control" placeholder="Высота (px)">
+            </div>
+    </div>
+
+
+    <wb-multiinput name="geopos">
+        <div class="col">
+            <input class="form-control" type="text" name="name" placeholder="Название">
+        </div>
+        <div class="col">
+            <input class="form-control" type="text" name="pos" placeholder="Координаты">
+        </div>
+        <div class="col-auto">
+            <input type="checkbox" class="wd-20 ht-20 m-2" name="active" />
+        </div>
+    </wb-multiinput>
+
+</edit>
+<view>
+<section>
+<div class="row">
+    <div class="px-5 col-12">
+        <h2 class="tx-semibold py-4 tx-40"  wb-if="'{{header}}'>''">
+            {{header}}
+        </h2>
+        <div class="tx-16 pb-3" wb-if="'{{text}}'>''">{{text}}</div>
+    </div>
+    <div class="col-12">
+    <div wb="module=yamap" zoom="{{zoom}}" height="{{height}}px">
+    <wb-foreach wb="from=geopos">
+        <geopos value="{{pos}}" title="{{name}}" wb-if="'{{active}}'=='on'"></geopos>
+    </wb-foreach>
+</div>
+</div>
+</div>
+</section>
+</view>
