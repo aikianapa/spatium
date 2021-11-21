a21cb0f3b32adb102add3ed0fd3c26121f15fbc6
Oleg Frolov <oleg_frolov@mail.ru>
2021-11-22 00:23:26 +0300

    programs price

--- blocks/programs.php
+++ blocks/programs.php
@@ -29,7 +29,12 @@
             </div>
             <div id="programsList">
             <div class="card-columns pt-4">
-            <wb-var prog="{'0':{'count':7,'name':'7 дней'},'1':{'count':14,'name':'14 дней'},'2':{'count':30,'name':'30 дней'}}" />
+            <wb-var prog="{
+                '0':{'count':3,'name':'3 дня'}
+                ,'1':{'count':7,'name':'7 дней'}
+                ,'2':{'count':14,'name':'14 дней'}
+                ,'3':{'count':30,'name':'30 дней'}
+            }" />
             <wb-foreach wb="{'table':'products',
                             'render':'server',
                             'limit':'3',
@@ -37,7 +42,7 @@
                             'rand':true,
                             'filter': {'category':'main','active':'on'}
                 }">
-                <div class="card bd-0">
+                <div class="card bd-0 mb-5">
                     <wb-var days="{{_var.prog.{{_idx}}.count}}" />
                     <wb-var discount="{{discounts.{{_var.days}}}}" wb-if="'{{discounts.{{_var.days}}}}'>'0'" else="1" />
                     <figure class="img-caption pos-relative mg-b-0">
@@ -47,7 +52,7 @@
                         <img data-src="/thumbc/350x300/src/{{images.{{_idx - (_var.cicle * _var.imgcnt - _var.imgcnt) }}.img}}" class="card-img-top object-cover" width="350" height="300"
                             alt="{{name}}">
                         <figcaption
-                            class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column justify-content-center bg-white-9 transition-base op-0">
+                            class="pos-absolute a-0 wd-100p pd-20 d-flex flex-column tx-10 tx-md-14 justify-content-md-center bg-white-9 transition-base op-0">
                             <h6 class="tx-inverse tx-semibold mg-b-20">{{name}}</h6>
                             <div class="tx-inverse mg-b-0">{{text}}</div>
                         </figcaption>
@@ -56,18 +61,26 @@
                     <div class="position-absolute t-10 l-10 avatar avatar-lg" wb-if="'{{_var.discount}}'<'1'">
                         <span class="avatar-initial rounded-circle bg-orange shadow-base">-{{(1-_var.discount)*100}}%</span>
                     </div>
-                    <div class="card-body text-center pb-0">
-                        <h5 class="tx-semibold">{{_var.prog.{{_idx}}.name}}</h5>
-                        <p>{{price * {{_var.days}} * {{_var.discount}} }} р.</p>
-                    </div>
-                    <div class="card-footer text-center bd-0 pt-0">
-                    <a href="javascript:void(0);" data-id="{{id}}"
-                            data-name="{{name}}" data-price="{{price}}"
-                            data-image="{{images.0.img}}" data-days="{{_var.days}}"
-                            data-discounts="{{json_encode({{discounts}})}}"
-                            data-link="/products/{{id}}/{{wbUrlOnly({{name}})}}"
-                            class="mod-cart-add mod-cart-data btn btn-success tx-semibold px-4 rounded-20">Заказать
-                            <img src="/module/myicons/shopping-cart.svg?size=26&stroke=FFFFFF" width="26" height="26"></a>
+                    <div class="card-body text-center pb-0 tx-sm-12 tx-md-14">
+                        <wb-foreach wb-from="_var.prog">
+                        <div class="row bd-b">
+                            <wb-var days="{{_var.prog.{{_idx}}.count}}" />
+                            <wb-var discount="{{_parent.discounts.{{_var.days}}}}" wb-if="'{{_parent.discounts.{{_var.days}}}}'>'0'" else="1" />
+                            <div class="col tx-left nobr">{{_var.prog.{{_idx}}.name}} <div class="badge badge-warning"  wb-if="'{{_var.discount}}'<'1'">
+                                    -{{(1-{{_var.discount}})*100}}%
+                                </div></div>
+                            <div class="col tx-right">{{_parent.price * {{_var.days}} * {{_var.discount}} }}₽</div>
+                            <div class="col-auto">
+                            <a href="javascript:void(0);" data-id="{{_parent.id}}"
+                            data-name="{{_parent.name}}" data-price="{{_parent.price}}"
+                            data-image="{{_parent.images.0.img}}" data-days="{{_var.days}}"
+                            data-discounts="{{json_encode({{_parent.discounts}})}}"
+                            data-link="/products/{{_parent.id}}/{{wbUrlOnly({{_parent.name}})}}"
+                            class="mod-cart-add mod-cart-data">
+                            <img src="/module/myicons/shopping-cart.svg?size=24&stroke=10b759" width="24" height="24"></a>
+                            </div>
+                        </div>
+                        </wb-foreach>
                     </div>
                 </div>
                 </wb-foreach>
