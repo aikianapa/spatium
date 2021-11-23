130f735f7fa821b52370477fcf294ba74d99cfbf
Oleg Frolov <oleg_frolov@mail.ru>
2021-11-23 11:39:15 +0300

    update

--- blocks/head.php
+++ blocks/head.php
@@ -15,8 +15,7 @@
   <link rel="preload" as="style" href="/engine/modules/datetimepicker/datetimepicker/bootstrap-datetimepicker.min.css" />
   <link rel="preload" as="style" href="/engine/modules/datetimepicker/datetimepicker.less" />
   <link rel="preload" as="style" href="/engine/lib/fonts/roboto/roboto.css" />
-  <link rel="preload" as="style" href="/assets/css/loader.scss" />
-  <link rel="preload" as="style" href="/assets/css/animate.css" />
+    <link rel="preload" as="style" href="/assets/css/animate.css" />
   <link rel="preload" as="style" href="/assets/css/custom.less" />
   
   <link rel="preload" as="script" href="/engine/lib/bootstrap/js/bootstrap.bundle.min.js" />
