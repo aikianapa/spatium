43be6abef898a075122aea4bacf13064742dd20a
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-13 12:03:58 +0300

    update

--- tpl/_cabinet.php
+++ tpl/_cabinet.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head></head>
-
-<body class="scroll-y">
-    <wb-module wb="module=yonger&mode=render&view=header" />
     <div class="my-account-section section position-relative mb-50 mg-t-80">
         <div class="container">
             <div class="row">
@@ -245,7 +238,3 @@
             </div>
         </div>
     </div>
-    <wb-module wb="module=yonger&mode=render&view=footer" />
-</body>
-
-</html>
