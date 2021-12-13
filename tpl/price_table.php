dd3655af6586d162d69e33d735965a2e888ec52c
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-13 14:28:51 +0300

    update

--- tpl/price_table.php
+++ tpl/price_table.php
@@ -7,12 +7,12 @@
                 -{{(1-{{_var.discount}})*100}}%
             </div>
         </div>
-        <div class="col-auto tx-right">{{_var.product.price * {{_key}} * {{_var.discount}} }}₽
+        <div class="col-auto tx-right">
             <a href="javascript:void(0);" data-id="{{_var.product.id}}" data-name="{{_var.product.name}}" data-price="{{_var.product.price}}" data-image="{{_var.product.images.0.img}}"
                 data-days="{{_key}}" data-link="/products/{{_var.product.id}}/{{wbUrlOnly({{_var.product.name}})}}"
-                class="mod-cart-add mod-cart-data ml-3">
+                class="mod-cart-add mod-cart-data">
+                <span class="tx-success d-none d-lg-inline"> {{_var.product.price * {{_key}} * {{_var.discount}} }}₽</span>
                 <img src="/module/myicons/shopping-cart.svg?size=24&stroke=10b759" width="24" height="24">
-                <span class="tx-success d-none d-lg-inline"> купить</span>
             </a>
         </div>
     </div>
