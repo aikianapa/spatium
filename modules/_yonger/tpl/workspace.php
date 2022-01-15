67b99864df195bd777db7806df2c9565503e8fed
Oleg Frolov <oleg_frolov@mail.ru>
2022-01-16 00:34:38 +0300

    remove old yonger

--- modules/_yonger/tpl/workspace.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body wb-if='in_array({{_sess.user.role}},explode(",","{{_sett.modules.yonger.allow}}"))'>
-    <wb-include wb="{'src':'/modules/yonger/tpl/ws_glob.php'}" wb-if=' "{{_route.subdomain}}" == "" OR "{{_sett.modules.yonger.standalone}}" == "on" ' />
-    <wb-include wb="{'src':'/modules/yonger/tpl/ws_site.php'}" wb-if=' "{{_route.subdomain}}" > ""  AND "{{_sett.modules.yonger.standalone}}" !== "on" ' />
-</body>
-<body wb-if="'{{_sess.user.role}}' == 'user'">
-    <wb-include wb="{'tpl':'cabinet.php'}" />
-</body>
-</html>
