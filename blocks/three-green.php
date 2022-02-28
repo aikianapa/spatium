266877ddb47e1dab21a77c2b6dc09a94cbe7de17
Oleg Frolov <oleg_frolov@mail.ru>
2022-02-28 22:56:43 +0300

    update

--- blocks/three-green.php
+++ blocks/three-green.php
@@ -22,7 +22,7 @@
         <wb-foreach wb="from=list&tpl=false">
           <li class="col-sm-6">
             <div class="media wd-sm-80p">
-              <img src="/assets/img/{{img}}.svg" class="mg-r-20" alt="">
+              <img data-src="/assets/img/{{img}}.svg" width="60" height="60" class="mg-r-20" alt="">
               <div class="media-body tx-18 tx-white pb-5">
                 {{text}}
               </div>
