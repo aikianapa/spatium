9e7d0d57c2deb6adc6641e7b42479a78b458e6dd
Oleg Frolov <oleg_frolov@mail.ru>
2022-04-28 17:36:05 +0300

    price_table

--- tpl/price_table.php
+++ tpl/price_table.php
@@ -4,16 +4,16 @@
 <wb-foreach wb-from="_var.disc" wb-tpl="false">
     <div class="row bd-b tx-normal">
         <wb-var discount="{{percent}}" wb-if="'{{percent}}'>'0'" else="1" />
-        <div class="col tx-left nobr">{{name}}
+        <div class="col tx-left nobr">{{name}}1
             <div class="badge badge-warning" wb-if="'{{_var.discount}}'<'1'">
                 -{{(1-{{_var.discount}})*100}}%
             </div>
         </div>
         <div class="col-auto tx-right">
+            <span> {{_var.product.price * {{_key}} * {{_var.discount}} }}₽</span>
             <a href="javascript:void(0);" data-id="{{_var.product.id}}" data-name="{{_var.product.name}}" data-price="{{_var.product.price}}" data-image="{{_var.product.images.0.img}}"
                 data-days="{{_key}}" data-link="/products/{{_var.product.id}}/{{wbUrlOnly({{_var.product.name}})}}"
                 class="mod-cart-add mod-cart-data">
-                <span class="tx-success"> {{_var.product.price * {{_key}} * {{_var.discount}} }}₽</span>
                 <img src="/module/myicons/shopping-cart.svg?size=24&stroke={{_var.colorSuccess}}" width="24" height="24">
             </a>
         </div>
