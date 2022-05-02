682d51d3529ec82dc734af35a257a86c6e26190e
Oleg Frolov <oleg_frolov@mail.ru>
2022-05-02 14:48:03 +0300

    update

--- tpl/price_table.php
+++ tpl/price_table.php
@@ -13,7 +13,7 @@
             <span> {{_var.product.price * {{_key}} * {{_var.discount}} }}₽</span>
             <a href="javascript:void(0);" data-id="{{_var.product.id}}" data-name="{{_var.product.name}}" data-price="{{_var.product.price}}" data-image="{{_var.product.images.0.img}}"
                 data-days="{{_key}}" data-link="/products/{{_var.product.id}}/{{wbUrlOnly({{_var.product.name}})}}"
-                class="mod-cart-add mod-cart-data">
+                class="mod-cart-add mod-cart-data cursor-pointer" onclick>
                 <img src="/module/myicons/shopping-cart.svg?size=24&stroke={{_var.colorSuccess}}" width="24" height="24">
             </a>
         </div>
@@ -22,7 +22,7 @@
 
 <wb-var uspr="{{_sess.user.promo}}" wb-if="'{{_sess.user.promo}}'>' '" else='[]'/>
 <wb-var class="mod-cart-add" wb-if="'{{_sess.user.id}}'>' '"  else="__mod-cart-add"/>
-<button class="btn btn-sm btn-block btn-outline-success rounded-20 my-3 btn-promo mod-cart-data {{_var.class}}"
+<button class="btn btn-sm btn-block btn-outline-success rounded-20 my-3 btn-promo mod-cart-data cursor-pointer {{_var.class}}"
  data-id="{{_var.product.id}}"
  data-name="{{_var.product.name}}"
  data-price="{{_var.product.promoprice}}"
@@ -30,6 +30,7 @@
  data-days="1"
  data-promo="1"
  data-link="/products/{{_var.product.id}}/{{wbUrlOnly({{_var.product.name}})}}"
+ onclick
  wb-if="'{{_var.product.promoprice}}'>' ' && !in_array('{{_var.product.id}}',{{_var.uspr}})">
  Попробовать за {{_var.product.promoprice}}₽
 </button>
